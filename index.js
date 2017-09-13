1c767c4bd3f217e66b5506fb962d4fbb1945b006
Xinlan Emily Hu <xehu@stanford.edu>
2017-09-13 17:26:34 -0400

    adding information to set up the about me, projects, and contact pages

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 $('#aboutMe').click(function(){
-	alert("coming soon");
+	console.log("clicked");
+	$('#aboutMePage').show();
+})
+
+$('#projects').click(function(){
+	console.log("clicked");
+	$('#projectsPage').show();
+})
+
+$('#contact').click(function(){
+	console.log("clicked");
+	$('#contactPage').show();
 })
