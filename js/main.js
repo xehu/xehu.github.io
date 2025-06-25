9d098d5d2923d00743a38cbfcaa66e216fa7321e
Xinlan Emily Hu <xehu@wharton.upenn.edu>
2025-06-25 18:15:17 -0400

    working carousel

--- js/main.js
+++ js/main.js
@@ -161,39 +161,18 @@
     var clSlickSlider = function() {
         
         $('.featured-slider').slick({
-            arrows: true,
-            dots: true,
-            infinite: true,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            centerMode: true,
-            centerPadding: '10%',
-            pauseOnFocus: false,
-            autoplaySpeed: 1500,
-            responsive: [
-                {
-                    breakpoint: 1400,
-                    settings: {
-                        arrows: false,
-                        centerPadding: '8%'
-                    }
-                },
-                {
-                    breakpoint: 900,
-                    settings: {
-                        arrows: false,
-                        centerPadding: '5%'
-                    }
-                },
-                {
-                    breakpoint: 400,
-                    settings: {
-                        arrows: false,
-                        centerMode: false
-                    }
-                }
-            ]
+          arrows: true,
+          dots: true,
+          infinite: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: false,      
+          centerPadding: '0',     
+          pauseOnFocus: false,
+          autoplay: true,
+          autoplaySpeed: 2000
         });
+
     };
 
 
@@ -251,37 +230,37 @@
     };
 
 
-   /* AjaxChimp
-    * ------------------------------------------------------ */
-    var clAjaxChimp = function() {
+   // /* AjaxChimp
+   //  * ------------------------------------------------------ */
+   //  var clAjaxChimp = function() {
         
-        $('#mc-form').ajaxChimp({
-            language: 'es',
-            url: cfg.mailChimpURL
-        });
+   //      $('#mc-form').ajaxChimp({
+   //          language: 'es',
+   //          url: cfg.mailChimpURL
+   //      });
 
-        // Mailchimp translation
-        //
-        //  Defaults:
-        //	 'submit': 'Submitting...',
-        //  0: 'We have sent you a confirmation email',
-        //  1: 'Please enter a value',
-        //  2: 'An email address must contain a single @',
-        //  3: 'The domain portion of the email address is invalid (the portion after the @: )',
-        //  4: 'The username portion of the email address is invalid (the portion before the @: )',
-        //  5: 'This email address looks fake or invalid. Please enter a real email address'
+   //      // Mailchimp translation
+   //      //
+   //      //  Defaults:
+   //      //	 'submit': 'Submitting...',
+   //      //  0: 'We have sent you a confirmation email',
+   //      //  1: 'Please enter a value',
+   //      //  2: 'An email address must contain a single @',
+   //      //  3: 'The domain portion of the email address is invalid (the portion after the @: )',
+   //      //  4: 'The username portion of the email address is invalid (the portion before the @: )',
+   //      //  5: 'This email address looks fake or invalid. Please enter a real email address'
 
-        $.ajaxChimp.translations.es = {
-            'submit': 'Submitting...',
-            0: '<i class="fas fa-check"></i> We have sent you a confirmation email',
-            1: '<i class="fas fa-exclamation-circle"></i> You must enter a valid e-mail address.',
-            2: '<i class="fas fa-exclamation-circle"></i> E-mail address is not valid.',
-            3: '<i class="fas fa-exclamation-circle"></i> E-mail address is not valid.',
-            4: '<i class="fas fa-exclamation-circle"></i> E-mail address is not valid.',
-            5: '<i class="fas fa-exclamation-circle"></i> E-mail address is not valid.'
-        } 
+   //      $.ajaxChimp.translations.es = {
+   //          'submit': 'Submitting...',
+   //          0: '<i class="fas fa-check"></i> We have sent you a confirmation email',
+   //          1: '<i class="fas fa-exclamation-circle"></i> You must enter a valid e-mail address.',
+   //          2: '<i class="fas fa-exclamation-circle"></i> E-mail address is not valid.',
+   //          3: '<i class="fas fa-exclamation-circle"></i> E-mail address is not valid.',
+   //          4: '<i class="fas fa-exclamation-circle"></i> E-mail address is not valid.',
+   //          5: '<i class="fas fa-exclamation-circle"></i> E-mail address is not valid.'
+   //      } 
 
-    };
+   //  };
 
 
    /* Back to Top
@@ -574,7 +553,7 @@
         clSmoothScroll();
         clAlertBoxes();
         clAOS();
-        clAjaxChimp();
+        // clAjaxChimp();
         clBackToTop();
         clGoogleMap();
 
